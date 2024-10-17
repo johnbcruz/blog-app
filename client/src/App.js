fc8b131953fe9777a424fb7d674776d411ebc474
jzurc7 <100167348+jzurc7@users.noreply.github.com>
2024-10-17 11:15:10 -0500

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import "./App.css";
+import Post from "./Post";
+// import Header from "./Header";
+import { Routes, Route } from "react-router-dom";
+import Layout from "./Layout";
+import IndexPage from "./pages/IndexPage";
+import LoginPage from "./pages/LoginPage";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<IndexPage />} />
+        <Route path={"/login"} element={<LoginPage />} />
+      </Route>
+    </Routes>
+  );
+}
+
+export default App;
---
+++ client/src/Post.js
@@ -0,0 +1,24 @@
+export default function Post() {
+  return (
+    <div className="post">
+      <div>
+        <img
+          src="https://techcrunch.com/wp-content/uploads/2023/01/IMG_2469-Large.jpeg"
+          alt=""
+        />
+      </div>
+      <div className="texts">
+        <h2>Full-house battery backup coming later this year</h2>
+        <p className="info">
+          <a className="author">John Cruz</a>
+          <time>2024-10-17 1:21</time>
+        </p>
+        <p className="summary">
+          Today at its special launch event, home backup power giant EcoFlow
+          launched a flurry of new products, including a 'Whole-home Backup
+          Power Solution.'
+        </p>
+      </div>
+    </div>
+  );
+}
